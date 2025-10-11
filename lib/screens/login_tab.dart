bbe6fb43773cb2106bf2c44bb9280cbe01e81be2
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2025-10-11 17:08:57 -0300

    Start 'login' tab body

--- lib/screens/login_tab.dart
+++ lib/screens/login_tab.dart
@@ -1,10 +1,91 @@
 import 'package:flutter/material.dart';
 
-class LoginTab extends StatelessWidget {
-	const LoginTab({super.key});
+import '../widgets/input_field.dart';
 
-	@override
-	Widget build(BuildContext context) {
-		return Center(child: Text('Login'));
-	}
+final Color darkBlue = Color(0xff002C53);
+final Color lightGray = Color(0xffEBEBEB);
+final Color green = Color(0xff009951);
+
+class LoginTab extends StatelessWidget {
+  const LoginTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2.5,
+        height: MediaQuery.of(context).size.height / 1.5,
+        decoration: BoxDecoration(
+          color: lightGray,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(50),
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 5,
+                children: <Widget>[
+                  Text('Email:', style: TextStyle(fontSize: 20)),
+                  InputField(
+                    labelText: 'Email',
+                    width: 500,
+                    enabledBorderColor: Colors.black.withAlpha(50),
+                    focusedBorderColor: darkBlue,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 5,
+                children: <Widget>[
+                  Text('Senha:', style: TextStyle(fontSize: 20)),
+                  InputField(
+                    labelText: 'Senha',
+                    width: 500,
+                    obscureText: true,
+                    enabledBorderColor: Colors.black.withAlpha(50),
+                    focusedBorderColor: darkBlue,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(60),
+              child: SizedBox(
+                width: 300,
+                height: 75,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: green,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    'Fazer Login',
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
