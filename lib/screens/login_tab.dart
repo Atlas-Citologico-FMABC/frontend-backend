0c2a205fe5ad8323bd078a6beff5638407756f98
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2025-10-14 12:09:28 -0300

    Add function to the button to validate the form
    Create a global form key and add the 'SingleChildScrollView' to the form to avoid height overflowing

--- lib/screens/login_tab.dart
+++ lib/screens/login_tab.dart
@@ -8,73 +8,84 @@ final Color lightGray = Color(0xffEBEBEB);
 final Color green = Color(0xff009951);
 
 class LoginTab extends StatelessWidget {
-	final Function(TabType) onTap;
+  final Function(TabType) onTap;
   LoginTab({super.key, required this.onTap});
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width / 2.5,
-        height: MediaQuery.of(context).size.height / 1.5,
-        decoration: BoxDecoration(
-          color: lightGray,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(50),
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 5,
-                children: <Widget>[
-                  Text('Email:', style: TextStyle(fontSize: 20)),
-                  InputField(
-                    labelText: 'Email',
-                    width: 500,
-                    enabledBorderColor: Colors.black.withAlpha(50),
-                    focusedBorderColor: darkBlue,
+    return Form(
+      key: _formKey,
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width / 2.5,
+          height: MediaQuery.of(context).size.height / 1.5,
+          decoration: BoxDecoration(
+            color: lightGray,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(50),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                   ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 5,
-                children: <Widget>[
-                  Text('Senha:', style: TextStyle(fontSize: 20)),
-                  InputField(
-                    labelText: 'Senha',
-                    width: 500,
-                    obscureText: true,
-                    enabledBorderColor: Colors.black.withAlpha(50),
-                    focusedBorderColor: darkBlue,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 5,
+                    children: <Widget>[
+                      Text('Email:', style: TextStyle(fontSize: 20)),
+                      InputField(
+                        labelText: 'Email',
+                        width: 500,
+                        enabledBorderColor: Colors.black.withAlpha(50),
+                        focusedBorderColor: darkBlue,
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 5,
+                    children: <Widget>[
+                      Text('Senha:', style: TextStyle(fontSize: 20)),
+                      InputField(
+                        labelText: 'Senha',
+                        width: 500,
+                        obscureText: true,
+                        enabledBorderColor: Colors.black.withAlpha(50),
+                        focusedBorderColor: darkBlue,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(60),
+                  child: Button(
+                    text: 'Fazer Login',
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        onTap(TabType.admin);
+                      }
+                    },
+                    foregroundColor: Colors.white,
+                    backgroundColor: green,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(60),
-              child: Button(
-                text: 'Fazer Login',
-                onTap: () => onTap(TabType.admin),
-                foregroundColor: Colors.white,
-                backgroundColor: green,
-                fontSize: 30,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
